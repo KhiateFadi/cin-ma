d907f6523f00718e46d03aa19a8e81dc89ec02bc
KhiateFadi <khiatefadi@gmail.com>
2020-05-06 17:05:29 +0200

    connexion fontionnelle

--- Cinema_World-HTML5/traitement/traitement_connexion.php
+++ Cinema_World-HTML5/traitement/traitement_connexion.php
@@ -2,11 +2,10 @@
 
 require('../modele/modele_connexion.php');
 require('../manager/manager_connexion.php');
+var_dump($_POST);
 
 
-
-
-$connexion = new connexion(['mail'=>$_POST['mail'], 'mdp'=>$_POST['mdp']]);
+$connexion = new connexion (['mail'=>$_POST['mail'], 'mdp'=>$_POST['mdp']]);
 $manager = new manager_connexion();
 $manager->connexion($connexion);
 
