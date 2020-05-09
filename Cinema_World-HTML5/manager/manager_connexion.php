19c9a922dd1bcf974935d49b5c53c2db9d32013c
KhiateFadi <khiatefadi@gmail.com>
2020-05-10 00:16:30 +0200

    correction de bug (la connexion étais bof)

--- Cinema_World-HTML5/manager/manager_connexion.php
+++ Cinema_World-HTML5/manager/manager_connexion.php
@@ -1,11 +1,11 @@
 <?php
-<<<<<<< HEAD
-//session_start();
-=======
+
+session_start();
+
 
 require_once('../modele/modele_connexion.php');
 require_once('../traitement/traitement_connexion.php');
->>>>>>> 1b7b164d67a734d284e04b12a2d3816d67a2e3d2
+
 
 Class Manager{
 
@@ -22,40 +22,31 @@ Class Manager{
          $_SESSION['prenom'] = $donne['prenom'];
 header('location: ../vue/admin.php');
 
-<<<<<<< HEAD
      }
      else{
          header('location: ../vue/moncompte.hmtl');
      }
  }
-=======
-  public function connexion($connexion){
-  $bdd= new PDO('mysql:host=localhost;dbname=cinema;charset=utf8','root','');
-  $reponse=$bdd->prepare('SELECT * FROM utilisateur WHERE mail=:mail AND mdp=:mdp');
-  $reponse->execute(array(
-  'mail'=>$connexion->getMail(),
-  'mdp'=>$connexion->getMdp()));
-  $donnee=$reponse->fetch();
-  var_dump($connexion);
->>>>>>> 1b7b164d67a734d284e04b12a2d3816d67a2e3d2
+
+  
 
 
 
 
-<<<<<<< HEAD
+
   public function afficheUser($id){
   $bdd = new PDO('mysql:host=localhost;dbname=cinema;charset=utf8','root','');
 
-=======
-        }
->>>>>>> 1b7b164d67a734d284e04b12a2d3816d67a2e3d2
+
+
+
 
 $red = $bdd->prepare('SELECT * FROM utilisateurs WHERE id=:id');
 $red->execute(array('id'=>$_SESSION['id']));
 $c = $red->fetch();
     return $c;
+}
 
-  }
   public function admin($all){
   $bdd = new PDO('mysql:host=localhost;dbname=cinema;charset=utf8','root','');
 
--- Cinema_World-HTML5/traitement/traitement_connexion.php
+++ Cinema_World-HTML5/traitement/traitement_connexion.php
@@ -1,5 +1,5 @@
 <?php
-<<<<<<< HEAD
+
 require('../modele/modele_connexion.php');
 require('../manager/manager_connexion.php');
 
@@ -7,19 +7,4 @@ $connexion = new connexion($_POST['mail'], $_POST['mdp']);
 $manager=new Manager();
 $manager->connexion($connexion);
 
-var_dump($connexion);
-=======
-session_start();
-require_once('../modele/modele_connexion.php');
-require_once('../manager/manager_connexion.php');
-
-
-
-$connexion = new connexion(['mail'=>$_POST['mail'],'mdp'=>$_POST['mdp']]);
-$manager = new manager_connexion();
-$manager->connexion($connexion);
-
-
-
->>>>>>> 1b7b164d67a734d284e04b12a2d3816d67a2e3d2
 ?>
