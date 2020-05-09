330e84dd25cb9c37dc853e260c7adfd48e99bbba
iliassalmi <56584061+iliassalmi@users.noreply.github.com>
2020-05-10 00:18:59 +0200

    fvdfg y

--- Cinema_World-HTML5/traitement/traitement_connexion.php
+++ Cinema_World-HTML5/traitement/traitement_connexion.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 require('../modele/modele_connexion.php');
 require('../manager/manager_connexion.php');
 
@@ -8,18 +7,4 @@ $manager=new Manager();
 $manager->connexion($connexion);
 
 var_dump($connexion);
-=======
-session_start();
-require_once('../modele/modele_connexion.php');
-require_once('../manager/manager_connexion.php');
-
-
-
-$connexion = new connexion(['mail'=>$_POST['mail'],'mdp'=>$_POST['mdp']]);
-$manager = new manager_connexion();
-$manager->connexion($connexion);
-
-
-
->>>>>>> 1b7b164d67a734d284e04b12a2d3816d67a2e3d2
 ?>
