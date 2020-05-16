1875926cda04a62c7ce132e10ba10322da88a9a2
KhiateFadi <khiatefadi@gmail.com>
2020-05-17 00:04:35 +0200

    mise a jour de la BDD (ajout d'une table)

--- Cinema_World-HTML5/cinema.sql
+++ Cinema_World-HTML5/cinema.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 16 mars 2020 à 12:39
--- Version du serveur :  5.7.26
--- Version de PHP :  5.6.40
+-- Généré le :  sam. 16 mai 2020 à 22:03
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,17 +25,63 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `avis`
+--
+
+DROP TABLE IF EXISTS `avis`;
+CREATE TABLE IF NOT EXISTS `avis` (
+  `pseudo` varchar(40) NOT NULL,
+  `commentaire` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE IF NOT EXISTS `contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(500) NOT NULL,
+  `mail` varchar(500) NOT NULL,
+  `message` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `contact`
+--
+
+INSERT INTO `contact` (`id`, `nom`, `mail`, `message`) VALUES
+(1, 'Khiate', 'rzetg', 'ezrtghn'),
+(2, 'Khiate', 'rzetg', 'ezrtghn'),
+(3, 'Khiate', 'rzetg', 'ezrtghn'),
+(4, 'Khiate', 'rzetg', 'ezrtghn'),
+(5, 'srhdt', 'reh', 'zy');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `reservation`
 --
 
 DROP TABLE IF EXISTS `reservation`;
 CREATE TABLE IF NOT EXISTS `reservation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(500) NOT NULL,
   `film` text NOT NULL,
   `nb_place` text NOT NULL,
   `3D` varchar(3) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `reservation`
+--
+
+INSERT INTO `reservation` (`id`, `nom`, `film`, `nb_place`, `3D`) VALUES
+(1, 'Khiate', 'yes', '2', 'non');
 
 -- --------------------------------------------------------
 
@@ -50,7 +96,55 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `mdp` varchar(500) NOT NULL,
   `num` varchar(500) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=43 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `mail`, `mdp`, `num`) VALUES
+(1, 'antouanevasone@gmail.com', 'YALAP', '0150403020'),
+(2, 'thomas', 'YALAP', '0150403020'),
+(3, '', '', ''),
+(4, '', '', ''),
+(5, 'antouanevasone@gmail.com', 'YALAP', '0150403020'),
+(6, '', '', ''),
+(7, '', '', ''),
+(8, '', '', ''),
+(9, '', '', ''),
+(10, '', '', ''),
+(11, '', '', ''),
+(12, '', '', ''),
+(13, '', '', ''),
+(14, '', '', ''),
+(15, '', '', ''),
+(16, '', '', ''),
+(17, '', '', ''),
+(18, '', '', ''),
+(19, '', '', ''),
+(20, '', '', ''),
+(21, '', '', ''),
+(22, '', '', ''),
+(23, '', '', ''),
+(24, '', '', ''),
+(25, '', '', ''),
+(26, '', '', ''),
+(27, '', '', ''),
+(28, '', '', ''),
+(29, '', '', ''),
+(30, '', '', ''),
+(31, '', '', ''),
+(32, '', '', ''),
+(33, '', '', ''),
+(34, '', '', ''),
+(35, '', '', ''),
+(36, '', '', ''),
+(37, '', '', ''),
+(38, '', '', ''),
+(39, '', '', ''),
+(40, '', '', ''),
+(41, '', '', ''),
+(42, '', '', '');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
